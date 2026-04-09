f02991054e6c26b5ba273bea33e0fdf35deb5ec9
Сергей <Serg18-124@yandex.ru>
2026-04-09 16:20:52 +0300

    Последняя версии рабочего окна

--- app/player_controller.py
+++ app/player_controller.py
@@ -1,1 +1,23 @@
-# Здесь будет работа с плеером Windows
+import asyncio
+from winrt.windows.media.control import (
+    GlobalSystemMediaTransportControlsSessionManager as MediaManager,
+)
+
+
+async def get_current_media_info():
+    sessions = await MediaManager.request_async()
+    current_session = sessions.get_current_session()
+
+    if current_session is None:
+        return None
+
+    info = await current_session.try_get_media_properties_async()
+
+    return {
+        "title": info.title,
+        "artist": info.artist,
+    }
+
+
+def get_current_media():
+    return asyncio.run(get_current_media_info())
