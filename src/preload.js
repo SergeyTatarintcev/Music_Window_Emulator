747d994af02ef0232e40bbd69edd0151c08c3e60
Сергей <Serg18-124@yandex.ru>
2026-04-09 14:20:25 +0300

    Все переделаю

--- src/preload.js
+++
@@ -1,7 +0,0 @@
-const { contextBridge, ipcRenderer } = require('electron')
-
-contextBridge.exposeInMainWorld('api', {
-  onTrackUpdate: (callback) => {
-    ipcRenderer.on('track-update', (event, data) => callback(data))
-  }
-})
